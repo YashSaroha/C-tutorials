d87e081882cea43a6dd07c46107a0b235448e950
unknown <yashsaroha966@gmail.com>
2023-05-14 22:47:04 +0530

    sorting folder added

---
+++ Sorting.c/descending_sort.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    int a[10],b,i,j;
+    printf("enter an array of 10 elements\n");
+    for(i=0 ; i<10 ; i++)
+    {
+        scanf("%d",&a[i]);
+    }
+
+    for(i=0 ; i<10 ; i++)
+    {
+        for(int j=i+1 ; j<10 ; j++)
+        {
+            if(a[i] < a[j])
+            {
+                b = a[i];
+                a[i] = a[j];
+                a[j] = b;
+            }
+        }
+    }
+
+    printf("after sorting in descending order\n");
+    for(i=0 ; i<10 ; i++)
+    {
+        printf("%d\n",a[i]);
+    }
+
+    return 0;
+}
